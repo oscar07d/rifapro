85e015c8e19d85473e47b2334d34bf3869eb0db3
OSCAR ROLDAN <123038408+oscar07d@users.noreply.github.com>
2025-08-26 17:04:37 -0500

    Update components.js

--- js/components.js
+++ js/components.js
@@ -180,8 +180,6 @@ export const getExploreView = (rafflesHTML) => `
 export const getRaffleCard = (raffle) => {
     const percentage = raffle.soldPercentage || 0;
     
-    // --- LÍNEA CORREGIDA ---
-    // Cambiamos 'methodValue' por 'methodObject' y accedemos a 'methodObject.method'
     const paymentIconsHTML = raffle.paymentMethods.map(methodObject => {
         const method = paymentMethods.find(p => p.value === methodObject.method);
         return method ? `<img src="${method.icon}" alt="${method.name}" title="${method.name}">` : '';
@@ -209,8 +207,15 @@ export const getRaffleCard = (raffle) => {
                 ${paymentIconsHTML}
             </div>
 
-            <a href="#/raffle/${raffle.id}" class="btn btn-secondary">Administrar</a>
-        </div>
+            <div class="raffle-card-actions">
+                <a href="#/raffle/${raffle.id}" class="btn btn-secondary">Administrar</a>
+                <button type="button" class="btn-icon btn-delete-raffle" title="Eliminar Rifa">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="24px" height="24px" fill="currentColor" viewBox="0 0 256 256">
+                        <path d="M216,48H176V40a24,24,0,0,0-24-24H104A24,24,0,0,0,80,40v8H40a8,8,0,0,0,0,16h8V208a16,16,0,0,0,16,16H192a16,16,0,0,0,16-16V64h8a8,8,0,0,0,0-16ZM96,40a8,8,0,0,1,8-8h48a8,8,0,0,1,8,8v8H96Zm96,168H64V64H192ZM112,104v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Zm48,0v64a8,8,0,0,1-16,0V104a8,8,0,0,1,16,0Z"></path>
+                    </svg>
+                </button>
+            </div>
+            </div>
     </div>
     `;
 };
@@ -329,6 +334,7 @@ export const getTicketModal = () => `
         </div>
     </div>
 `;
+
 
 
 
