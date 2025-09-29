fb17e563588c81fcc1c8a9f178a6b09600e4a757
OSCAR ROLDAN <123038408+oscar07d@users.noreply.github.com>
2025-09-29 15:47:19 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -5,7 +5,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCWsjPbPZ2F9kU5fv-A9KCjKMFstjZgJKc",
   authDomain: "rifapro-6d8e9.firebaseapp.com",
   projectId: "rifapro-6d8e9",
-  storageBucket: "rifapro-6d8e9.firebasestorage.app",
+  storageBucket: "rifapro-6d8e9.appspot.com",
   messagingSenderId: "551676822561",
   appId: "1:551676822561:web:d3c8baa2c3ac1fddf80f60",
   measurementId: "G-Q5T3K9ZRNF"
