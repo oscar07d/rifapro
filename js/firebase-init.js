31d2f8b4b1af0d72d2c69ce91b1ddccbbb39508c
OSCAR ROLDAN <123038408+oscar07d@users.noreply.github.com>
2025-10-02 00:19:05 -0500

    Update firebase-init.js

--- js/firebase-init.js
+++ js/firebase-init.js
@@ -1,17 +1,16 @@
 // js/firebase-init.js
-import { initializeApp } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { initializeApp, getApps } from "https://www.gstatic.com/firebasejs/9.23.0/firebase-app.js";
+import { firebaseConfig } from "./firebase-config.js";
 
-// ⚠️ Ojo: storageBucket debe terminar en .appspot.com, no .app
-const firebaseConfig = {
-  apiKey: "AIzaSyCWsjPbPZ2F9kU5fv-A9KCjKMFstjZgJKc",
-  authDomain: "rifapro-6d8e9.firebaseapp.com",
-  projectId: "rifapro-6d8e9",
-  storageBucket: "rifapro-6d8e9.appspot.com",
-  messagingSenderId: "551676822561",
-  appId: "1:551676822561:web:d3c8baa2c3ac1fddf80f60",
-  measurementId: "G-Q5T3K9ZRNF"
-};
+// Evitar inicializar más de una vez
+let app;
+if (!getApps().length) {
+  app = initializeApp(firebaseConfig);
+  console.log("Firebase inicializado:", firebaseConfig.apiKey ? { apiKey: firebaseConfig.apiKey, projectId: firebaseConfig.projectId } : "sin apiKey");
+} else {
+  // si por alguna razón ya existe una app, la reutilizamos
+  app = getApps()[0];
+  console.log("Reutilizando app existente");
+}
 
-// Inicializa Firebase y expórtala
-const app = initializeApp(firebaseConfig);
 export { app };
