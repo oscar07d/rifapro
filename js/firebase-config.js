98da5a457eb72f8e142e48b8e1c8521661c4981f
OSCAR ROLDAN <123038408+oscar07d@users.noreply.github.com>
2025-10-02 00:19:20 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -1,15 +1,12 @@
 // js/firebase-config.js
-
-// Tu configuración personal de Firebase
 const firebaseConfig = {
   apiKey: "AIzaSyCWsjPbPZ2F9kU5fv-A9KCjKMFstjZgJKc",
   authDomain: "rifapro-6d8e9.firebaseapp.com",
   projectId: "rifapro-6d8e9",
-  storageBucket: "rifapro-6d8e9.appspot.com",
+  storageBucket: "rifapro-6d8e9.appspot.com", // ✅ ESTA ES LA CORRECTA
   messagingSenderId: "551676822561",
   appId: "1:551676822561:web:d3c8baa2c3ac1fddf80f60",
   measurementId: "G-Q5T3K9ZRNF"
 };
 
-// Exportamos la configuración para usarla en otros archivos
 export { firebaseConfig };
