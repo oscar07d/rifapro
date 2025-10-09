7ddadf483ee690624058c59de04568cca8d6b61b
OSCAR ROLDAN <123038408+oscar07d@users.noreply.github.com>
2025-10-09 00:02:52 -0500

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -3,7 +3,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyCWsjPbPZ2F9kU5fv-A9KCjKMFstjZgJKc",
   authDomain: "rifapro-6d8e9.firebaseapp.com",
   projectId: "rifapro-6d8e9",
-  storageBucket: "rifapro-6d8e9.appspot.com", // ✅ ESTA ES LA CORRECTA
+  storageBucket: "rifapro-6d8e9.firebasestorage.app", // ✅ ESTA ES LA CORRECTA
   messagingSenderId: "551676822561",
   appId: "1:551676822561:web:d3c8baa2c3ac1fddf80f60",
   measurementId: "G-Q5T3K9ZRNF"
